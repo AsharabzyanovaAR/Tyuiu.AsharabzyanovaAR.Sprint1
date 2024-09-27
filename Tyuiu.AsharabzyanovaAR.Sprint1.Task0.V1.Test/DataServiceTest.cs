6c1ee9e379c26a797d0a6b9795b9b653160a6a5c
AsharabzyanovaAR <rustamovna14082006@gmail.com>
2024-09-27 14:34:11 +0500

    Add project files.

---
+++ Tyuiu.AsharabzyanovaAR.Sprint1.Task0.V1.Test/DataServiceTest.cs
@@ -0,0 +1,16 @@
+﻿using Tyuiu.AsharabzyanovaAR.Sprint1.Task0.V1.Lib;
+
+namespace Tyuiu.AsharabzyanovaAR.Sprint1.Task0.V1.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExpression()
+        {
+            DataService ds = new DataService();
+            var res = ds.Calculate();
+            Assert.AreEqual((-6), res);
+        }
+    }
+}
