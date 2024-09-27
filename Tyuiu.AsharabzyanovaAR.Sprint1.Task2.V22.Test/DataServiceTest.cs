5737fd3054ed8fd1d6abe853cb4ae2492ebc10ae
AsharabzyanovaAR <rustamovna14082006@gmail.com>
2024-09-27 15:31:54 +0500

    Task2

---
+++ Tyuiu.AsharabzyanovaAR.Sprint1.Task2.V22.Test/DataServiceTest.cs
@@ -0,0 +1,18 @@
+﻿using Tyuiu.AsharabzyanovaAR.Sprint1.Task2.V22.Lib;
+namespace Tyuiu.AsharabzyanovaAR.Sprint1.Task2.V22.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExpression()
+        {
+            DataService ds = new DataService();
+            int x = 2;
+            int y = 3;
+            int z = 4;
+            var res = ds.CalculateAVGValue(x, y, z);
+            Assert.AreEqual(3, res);
+        }
+    }
+}
---
+++ Tyuiu.AsharabzyanovaAR.Sprint1.Task2.V22/Program.cs
@@ -0,0 +1,47 @@
+﻿using Tyuiu.AsharabzyanovaAR.Sprint1.Task2.V22.Lib;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        DataService ds = new DataService();
+        Console.Title = ("Спринт #1 | Выполнила: Ашарабзянова А.Р. | ПКТб-24-1");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* Спринт #1                                                               *");
+        Console.WriteLine("* Тема: Базовые навыки работы в С#                                        *");
+        Console.WriteLine("* Задание #2                                                              *");
+        Console.WriteLine("* Вариант #22                                                             *");
+        Console.WriteLine("* Выполнила: Ашарабзянова Алина Рустамовна | ПКТб-24-1                    *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* УСЛОВИЕ:                                                                *");
+        Console.WriteLine("* Написать программу, которая запрашивает у пользователя исходные данные, *");
+        Console.WriteLine("* выполняет указанные расчёты и печатает результат на экране.             *");
+        Console.WriteLine("*                                                                         *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+        Console.WriteLine("***************************************************************************");
+
+        int x;
+
+        Console.WriteLine("Введите значение X:");
+        x = Convert.ToInt32(Console.ReadLine());
+
+        int y;
+
+        Console.WriteLine("Введите значениеY :");
+        y = Convert.ToInt32(Console.ReadLine());
+
+        int z;
+
+        Console.WriteLine("Введите значение Z:");
+        z = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+        Console.WriteLine("***************************************************************************");
+
+        Console.WriteLine("Среднее значение X Y Z-" + ds.CalculateAVGValue(x, y, z));
+
+        Console.ReadLine();
+    }
+}
