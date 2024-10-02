f6a706f2a539e1cf8764adcb438cae462bb32fbc
AsharabzyanovaAR <rustamovna14082006@gmail.com>
2024-10-03 04:28:07 +0500

    Task7

---
+++ Tyuiu.AsharabzyanovaAR.Sprint1.Task7.V28.Test/DataServiceTest.cs
@@ -0,0 +1,21 @@
+﻿using Tyuiu.AsharabzyanovaAR.Sprint1.Task7.V28.Lib;
+using System.Net.WebSockets;
+
+namespace Tyuiu.AsharabzyanovaAR.Sprint1.Task7.V28.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExpression()
+        {
+            DataService ds = new DataService();
+            double x = 2;
+            double y = 3;
+            double wait =9.787;
+            Assert.AreEqual(wait, ds.Calculate(x, y));
+
+            
+        }
+    }
+}
---
+++ Tyuiu.AsharabzyanovaAR.Sprint1.Task7.V28/Program.cs
@@ -0,0 +1,42 @@
+﻿using Tyuiu.AsharabzyanovaAR.Sprint1.Task7.V28.Lib;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        DataService ds = new DataService();
+        Console.Title = ("Спринт #1 | Выполнила: Ашарабзянова А.Р. | ПКТб-24-1");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* Спринт #1                                                               *");
+        Console.WriteLine("* Тема: Базовые навыки работы в С#                                        *");
+        Console.WriteLine("* Задание #7                                                              *");
+        Console.WriteLine("* Вариант #28                                                             *");
+        Console.WriteLine("* Выполнила: Ашарабзянова Алина Рустамовна | ПКТб-24-1                    *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* УСЛОВИЕ:                                                                *");
+        Console.WriteLine("*Написать программу, которая вычисляет математическое выражение по        *");
+        Console.WriteLine("*исходным значениям данных, вводимых пользователем.                       *");
+        Console.WriteLine("*                                                                         *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("   2     1   x");
+        Console.WriteLine("cos (sin -)+y ");
+        Console.WriteLine("         x");
+
+        double x, y;
+
+        Console.WriteLine("Введите значение X:");
+        x = Convert.ToDouble(Console.ReadLine());
+
+        Console.WriteLine("Введите значение X:");
+        y = Convert.ToDouble(Console.ReadLine());
+
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+        Console.WriteLine("***************************************************************************");
+
+        Console.WriteLine(ds.Calculate(x, y));
+        Console.ReadKey();
+
+    }
+}
